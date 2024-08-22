d2d1f9b3c35e301183facfc987a5e515bfa79a43
Sam Gardner <samuel.gardner@ttu.edu>
2024-08-22 05:24:23 -0500

    transform satellite data to PlateCarree

--- geocolor.py
+++ geocolor.py
@@ -8,17 +8,17 @@ from matplotlib import pyplot as plt
 import numpy as np
 from cartopy import crs as ccrs
 from cartopy import feature as cfeat
-import metpy
-from os import path
-import pandas as pd
+from os import path, remove
 from siphon.catalog import TDSCatalog
 from os import path
 from pathlib import Path
 from datetime import datetime as dt, timedelta
 import json
 import sys
+from pyxlma import coords
+from timeoutcontext import timeout
 
-axExtent = [-130, -60, 20, 50]
+axExtent = [-125, -65, 24, 50]
 basePath = path.dirname(path.abspath(__file__))
 if path.exists(path.join(basePath, "HDWX_helpers.py")):
     import HDWX_helpers
@@ -41,26 +41,44 @@ def plotSat():
         lastPlottedTime = dt.strptime(currentRunMetadata["productFrames"][-1]["valid"], "%Y%m%d%H%M")
         if lastPlottedTime >= latestTimeAvailable:
             exit()
-    channel01 = dataAvail.remote_access(use_xarray=True)
-    channel02 = dataAvail2.remote_access(use_xarray=True)
-    channel03 = dataAvail3.remote_access(use_xarray=True)
+    with timeout(30):
+        channel01 = dataAvail.download(filename='./ch01.nc')
+        channel01 = xr.open_dataset('ch01.nc', chunks='auto')
+        channel02 = dataAvail2.download(filename='./ch02.nc')
+        channel02 = xr.open_dataset('ch02.nc', chunks='auto')
+        channel03 = dataAvail3.download(filename='./ch03.nc')
+        channel03 = xr.open_dataset('ch03.nc', chunks='auto')
+    
     blue = channel01.Sectorized_CMI.data
     red = channel02.Sectorized_CMI.data
     green = channel03.Sectorized_CMI.data
     pngdata = np.dstack([red/np.max(red), green/np.max(green), blue/np.max(blue)])
     
+    satsys = coords.GeostationaryFixedGridSystem(subsat_lon=channel01.satellite_longitude, sweep_axis='x')
+    geosys = coords.GeographicSystem()
+    x2d, y2d = np.meshgrid(channel01.x/1e6, channel01.y/1e6)
+    lon, lat, alt = geosys.fromECEF(*satsys.toECEF(x2d, y2d, np.zeros_like(x2d)))
+    lon.shape = x2d.shape
+    lat.shape = x2d.shape
+
+    for i in range(3):
+        pngdata[:, :, i] = np.where(np.abs(lon) == np.inf, np.nan, pngdata[:, :, i])
+        pngdata[:, :, i] = np.where(np.abs(lat) == np.inf, np.nan, pngdata[:, :, i])
+    lon[np.abs(lon) == np.inf] = np.nanmin(lon)
+    lat[np.abs(lat) == np.inf] = -99
+    lat[np.abs(lat) == -99] = np.nanmax(lat)
     
-    cfmetadata = channel01.metpy.parse_cf("Sectorized_CMI")
-    satProj = cfmetadata.metpy.cartopy_crs
-    mapProj = ccrs.epsg(3857)
-    img_extent = [cfmetadata.x.min(), cfmetadata.x.max(), cfmetadata.y.min(), cfmetadata.y.max()]
-    validTime = pd.to_datetime(cfmetadata.time.data)
+    validTime = dt.strptime(channel01.start_date_time, '%Y%j%H%M%S')
 
     fig = plt.figure()
-    ax = plt.axes(projection=satProj)
-    ax.imshow(pngdata, transform=satProj, extent=img_extent)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.pcolormesh(lon, lat, pngdata, transform=ccrs.PlateCarree())
     ax.add_feature(cfeat.COASTLINE.with_scale("50m"), linewidth=1, edgecolor="black", zorder=10)
     ax.add_feature(cfeat.STATES.with_scale("50m"), linewidth=0.5, edgecolor="black")
+    px = 1/plt.rcParams["figure.dpi"]
+    fig.set_size_inches(3840*px, 2160*px)
+    ax.set_extent(axExtent, crs=ccrs.PlateCarree())
+
     outputPath = path.join(basePath, "output", "products", "satellite", "goes16", "geocolor", validTime.strftime("%Y"), validTime.strftime("%m"), validTime.strftime("%d"), validTime.strftime("%H00"), validTime.strftime("%M.png"))
     Path(path.dirname(outputPath)).mkdir(parents=True, exist_ok=True)
     if hasHelpers:
@@ -74,22 +92,22 @@ def plotSat():
 
     if "--no-gis" not in sys.argv:
         gisFig = plt.figure()
-        gisAx = plt.axes(projection=mapProj)
+        gisAx = plt.axes(projection=ccrs.PlateCarree())
         gisAx.set_extent(axExtent, crs=ccrs.PlateCarree())
-        gisAx.imshow(pngdata, transform=satProj, interpolation="none", extent=img_extent)
+        gisAx.pcolormesh(lon, lat, pngdata, transform=ccrs.PlateCarree())
         gisOutputPath = path.join(basePath, "output", "gisproducts", "satellite", "goes16", "geocolor", validTime.strftime("%Y"), validTime.strftime("%m"), validTime.strftime("%d"), validTime.strftime("%H00"), validTime.strftime("%M.png"))
 
 
         Path(path.dirname(gisOutputPath)).mkdir(parents=True, exist_ok=True)
         gisAx.set_position([0, 0, 1, 1])
-        px = 1/plt.rcParams["figure.dpi"]
         gisFig.set_size_inches(3840*px, 2160*px)
         extent = gisAx.get_tightbbox(gisFig.canvas.get_renderer()).transformed(gisFig.dpi_scale_trans.inverted())
         gisFig.savefig(gisOutputPath, transparent=True, bbox_inches=extent)
         if hasHelpers:
             HDWX_helpers.writeJson(path.abspath(path.dirname(__file__)), 4, runTime=(validTime - timedelta(minutes=validTime.minute)), fileName=validTime.strftime("%M.png"), validTime=validTime, gisInfo=["0,0", "0,0"], reloadInterval=270)
 
-
+    for i in range(1, 4):
+        remove(f'ch0{i}.nc')
 
 if __name__ == "__main__":
     plotSat()
